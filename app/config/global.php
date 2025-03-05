d204db010364410434bd92578344874c1ffcd989
voidjuan <juan_elopez2@soy.sena.edu.co>
2025-03-05 11:46:45 -0500

    Configuracion de entorno Infinity

--- app/config/global.php
+++ app/config/global.php
@@ -1,9 +1,18 @@
 <?php
+// define("MAIN_APP_ROUTE", __DIR__.'/../app/');
+// define("DRIVER", 'mysql');
+// define("HOST", 'localhost');
+// define("USERNAME_DB", 'root');
+// define("PASSWORD_DB", '');
+// define("DATABASE", 'gym_cpic');
+// define("CHARTSET", 'utf8mb4');
+// define("COLLATION", 'utf8mb4_unicode_ci');
+
 define("MAIN_APP_ROUTE", __DIR__.'/../app/');
 define("DRIVER", 'mysql');
-define("HOST", 'localhost');
-define("USERNAME_DB", 'root');
-define("PASSWORD_DB", '');
-define("DATABASE", 'gym_cpic');
+define("HOST", 'sql308.infinityfree.com');
+define("USERNAME_DB", 'if0_38451927');
+define("PASSWORD_DB", 'lhdF19doACKfA9');
+define("DATABASE", 'if0_38451927_gym_cpic');
 define("CHARTSET", 'utf8mb4');
 define("COLLATION", 'utf8mb4_unicode_ci');
