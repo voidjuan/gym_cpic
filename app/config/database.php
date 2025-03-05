1c5f820c651d8a9c193714c024b24d0a941e1cea
voidjuan <juan_elopez2@soy.sena.edu.co>
2025-03-05 17:52:49 -0500

    cambios

---
+++ app/config/database.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    "driver" => "mysql",
+    "host" => "localhost",
+    "username" => "root",
+    "password" => "",
+    "database" => "gym_cpic",
+    "charset" => "utf8mb4",
+    "collation" => "utf8mb4_unicode_ci",
+];
